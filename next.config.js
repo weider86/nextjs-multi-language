0981a2d8e89c440c29c7ac2052add934e43f593f
weider86 <weider86@gmail.com>
2022-05-17 14:47:55 -0300

    finish initial config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    // providing the locales supported by your application
+    locales: ['en-US', 'es-ES', 'pt-BR'],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: 'en-US',
+  },
+};
